d947be887348be737f31428a3cee50eb58d0e4e4
Alonesa <alenakrasnov@mail.ru>
2022-10-13 17:45:46 +0300

    make test for a task filtrator reducer

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -2,6 +2,7 @@ import {TodolistsType} from '../App';
 import {v1} from 'uuid';
 import {todolistsReducer} from './todolists-reducer';
 
+
 test('todolist should be added', ()=>{
   let todolistID1 = v1();
   let todolistID2 = v1();
@@ -31,10 +32,29 @@ test('Todolist should be removed', ()=> {
   ]
   
   const finalState = todolistsReducer(initialState, {
-    type: 'REMOVE-TODOLIST',
+	type: 'REMOVE-TODOLIST',
 	id: todolistID1
   })
   
   expect(finalState.length).toBe(1)
   expect(finalState[0].id).toBe(todolistID2)
+})
+
+test(`Todolist filter can't be empty`, () => {
+  let todolistID1 = v1();
+  let todolistID2 = v1();
+  
+  const initialState: Array<TodolistsType> = [
+	{id: todolistID1, title: 'What to learn', filter: 'All'},
+	{id: todolistID2, title: 'What to buy', filter: 'All'}
+  ]
+  
+  const finalState = todolistsReducer(initialState, {
+	type: 'TASKS-FILTRATOR',
+	status: initialState[0].filter,
+	todolistId: todolistID1
+  })
+  
+  expect(finalState[0].filter).toBe('All')
+  expect(finalState[0].id).toBe(todolistID1)
 })
