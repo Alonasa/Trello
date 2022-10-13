b177b9a5206d38b9a5e002cad4a3d2cc40129760
Alonesa <alenakrasnov@mail.ru>
2022-10-13 16:01:37 +0300

    added remove todolist reducer

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,11 +1,16 @@
 import { TodolistsType } from '../App';
 import {v1} from 'uuid';
 
-export type TodolistsACType = AddTodolistAT
+export type TodolistsACType = AddTodolistAT | RemoveTodolistAT
 
 type AddTodolistAT = {
   type: "ADD-TODOLIST"
   title: string
+}
+
+type  RemoveTodolistAT = {
+  type: "REMOVE-TODOLIST"
+  id: string
 }
 
 export const todolistsReducer = (todolists: Array<TodolistsType>, action: TodolistsACType): Array<TodolistsType>=>{
@@ -13,6 +18,8 @@ export const todolistsReducer = (todolists: Array<TodolistsType>, action: Todoli
 	  case 'ADD-TODOLIST':
 	    const newTodolistId:string = v1();
 		return [{id: newTodolistId, title:action.title, filter: 'All'}, ...todolists]
+	  case 'REMOVE-TODOLIST':
+	    return  todolists.filter(t => t.id !== action.id)
 	  default: return todolists
 	}
 }
