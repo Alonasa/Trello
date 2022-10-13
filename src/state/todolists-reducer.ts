0525d2d93bddfa3a05796945235c1316e8ba6bcf
Alonesa <alenakrasnov@mail.ru>
2022-10-13 16:45:29 +0300

    added reducer for a tasks filtrator

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,7 +1,8 @@
 import { TodolistsType } from '../App';
 import {v1} from 'uuid';
+import {TasksStatusType} from '../components/Todolist/Todolist';
 
-export type TodolistsACType = AddTodolistAT | RemoveTodolistAT
+export type TodolistsACType = AddTodolistAT | RemoveTodolistAT | TasksFiltratorAT
 
 type AddTodolistAT = {
   type: "ADD-TODOLIST"
@@ -13,6 +14,12 @@ type  RemoveTodolistAT = {
   id: string
 }
 
+type TasksFiltratorAT = {
+  type: "TASKS-FILTRATOR"
+  status: TasksStatusType
+  todolistId: string
+}
+
 export const todolistsReducer = (todolists: Array<TodolistsType>, action: TodolistsACType): Array<TodolistsType>=>{
 	switch (action.type) {
 	  case 'ADD-TODOLIST':
@@ -20,6 +27,13 @@ export const todolistsReducer = (todolists: Array<TodolistsType>, action: Todoli
 		return [{id: newTodolistId, title: action.title, filter: 'All'}, ...todolists]
 	  case 'REMOVE-TODOLIST':
 	    return  todolists.filter(t => t.id !== action.id)
+	  
+	  case 'TASKS-FILTRATOR':
+	    return todolists.map(t => t.id === action.todolistId ? {
+		  ...t,
+		  filter: action.status
+		} : t)
+	  
 	  default: return todolists
 	}
 }
